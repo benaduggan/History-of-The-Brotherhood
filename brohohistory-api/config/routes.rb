3c4d0f538830f1713b9f4bf6448a0e65a867e87a
adam7700 <adampog77@gmail.com>
2016-04-20 23:32:25 -0400

    cleaned up stuff, added postgres init script.

--- brohohistory-api/config/routes.rb
+++ brohohistory-api/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-  get 'tests', to: 'tests#index'
-  put 'tests/:id(.:format)', to: 'tests#update'
 
 
 end
