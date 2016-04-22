1ec13e544f6ea83201f96ee9ca70a5d0165b0ea7
Ben Duggan <benaduggan@gmail.com>
2016-04-21 22:29:13 -0400

    adding angular client

---
+++ angular-client/src/app/components/navbar/navbar.directive.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularClient')
+    .directive('navbar', navbar);
+
+  /** @ngInject */
+  function navbar() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/navbar/navbar.html',
+      scope: {
+          creationDate: '='
+      },
+      controller: NavbarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function NavbarController(moment) {
+      var vm = this;
+
+    }
+  }
+
+})();
---
+++ angular-client/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularClient')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
