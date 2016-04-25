90263e46a48fd35c4918a60a672c8284d2db16ac
Ben Duggan <benaduggan@gmail.com>
2016-04-25 19:40:09 -0400

    starting to develop the initial data loading script

--- brohohistory-api/db/seeds.rb
+++ brohohistory-api/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+def get_or_create_floor
+  floor_id = ActiveRecord::Base.connection.execute("SELECT id FROM floor WHERE name = 'The Brotherhood'")
+  if floor_id.nil?
+    query = "INSERT INTO floor (name, description) VALUES ('The Brotherhood', 'An awesome place to live')"
+    x = ActiveRecord::Base.connection.execute(query)
+    return x.first[0]
+  else
+    return floor_id.first['id']
+  end
+end
+
+def create_rooms
+  single_rooms = [314, 315, 316, 317, 331, 332] # check that this is true
+  floor_id = get_or_create_floor()
+  (301...345).each do |room_number|
+    if single_rooms.include? room_number
+      num_occupants = 1
+    else
+      num_occupants = 2
+    end
+    query = "INSERT INTO room (floor_id, room_num, num_occupants) VALUES (#{floor_id}, #{room_number}, #{num_occupants})"
+    ActiveRecord::Base.connection.execute(query)
+  end
+end
