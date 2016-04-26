fb17fa12e52697d0cbdc13812fa69dc42a4e285f
Ben Duggan <benaduggan@gmail.com>
2016-04-25 21:18:14 -0400

    removing floor detail because we aren't going to bother in this iteration

--- angular-client/src/app/floor/floor.routes.js
+++ angular-client/src/app/floor/floor.routes.js
@@ -12,11 +12,6 @@
         url: '/floor/',
         templateUrl: 'app/floor/floorList.html',
         controller: 'FloorListController as vm'
-      })
-      .state('floorDetail', {
-        url: '/floor/:id/',
-        templateUrl: 'app/floor/floorDetail.html',
-        controller: 'FloorDetailController as vm'
       });
     }
 
