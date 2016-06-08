a925e1dd2141a13b9c0948292aba29a0a00906a6
Ben Duggan <benaduggan@gmail.com>
2016-06-08 17:08:08 -0400

    accounts looks done

--- brohohistory-api-django/app/accounts/urls.py
+++ brohohistory-api-django/app/accounts/urls.py
@@ -7,8 +7,9 @@ detail = {'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}
 
 urlpatterns = [
         url(r'^$', AccountViewSet.as_view(list), name="AccountList"),
-        # url(r'^me/$', CurrentUserView.as_view(), name="CurrentUser"),
+        url(r'^(?P<pk>\d+)/$', AccountViewSet.as_view(detail), name='Account'),
 
-        # url(r'^account/all/$', AccountViewSet.as_view({'get': 'list_all'}), name="AccountListAll"),
-        # url(r'^account/(?P<pk>\d+)/$', AccountViewSet.as_view(detail), name='Account'),
+        url(r'^all/$', AccountViewSet.as_view({'get': 'list_all'}), name="AccountListAll"),
+        url(r'^me/$', CurrentUserView.as_view(), name="CurrentUser"),
+
 ]
